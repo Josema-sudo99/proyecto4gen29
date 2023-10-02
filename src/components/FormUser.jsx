4cfc962c987530de98ac1fe829f2734a2a4271e9
Josema-sudo99 <josemmm010203@gmail.com>
2023-10-02 17:37:12 -0500

    repositorios

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -1,66 +1,74 @@
-import { useEffect } from 'react';
-import {useForm} from 'react-hook-form';
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import '../assets/style/FormUser.css';
 
+const FormUser = ({
+  createUser,
+  infoUpdate,
+  updateUser,
+  setInfoUpdate,
+  styleMobal,
+  handleForm2,
+}) => {
+  const { handleSubmit, register, reset } = useForm();
 
-const FormUser = ({createUser , infoUpdate , updateUser ,setInfoUpdate , styleMobal , handleForm2}) => {
+  useEffect(() => {
+    reset(infoUpdate);
+  }, [infoUpdate, reset]);
 
-   const {handleSubmit,register,reset} = useForm()
-
-   useEffect(() => {
-     reset(infoUpdate)
-   }, [infoUpdate])
-
-   const Submit = data => {
-    if(infoUpdate) {
-      //Editar
-      updateUser('/users', infoUpdate.id, data)
-      setInfoUpdate()
-    }else{
-      //Crear
-      createUser('/users',data)
+  const onSubmit = (data) => {
+    if (infoUpdate) {
+      // Editar usuario
+      updateUser('/users', infoUpdate.id, data);
+      setInfoUpdate(null); // Limpiar la información de actualización
+    } else {
+      // Crear usuario
+      createUser('/users', data);
     }
-    createUser('/users',data) 
-     reset({
-        email: '',
-        password: '',
-        first_name: '',
-        last_name: '' ,
-        bitthday: ''
-     })
-   }
+
+    // Restablecer el formulario después de enviar los datos
+    reset({
+      email: '',
+      password: '',
+      first_name: '',
+      last_name: '',
+      birthday: '',
+    });
+  };
 
   return (
     <div className={`${styleMobal} contaioner-form`}>
-    <form onSubmit={handleSubmit(Submit)} className="form">
-    <button  className='button-delete-usuario' onClick={handleForm2}>❎</button>
-    <div className='form-text'>
-    <h2 className='titulo-from'>New User</h2>
-        <div className='registre'>
-            <label className='registre-text' htmlFor="email">Email <br/></label>
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
+        <button className='button-delete-usuario' onClick={handleForm2}>❎</button>
+        <div className='form-text'>
+          <h2 className='titulo-from'>{infoUpdate ? 'Edit User' : 'New User'}</h2>
+          <div className='registre'>
+            <label className='registre-text' htmlFor="email">Email <br /></label>
             <input className='registre-cuadro' {...register('email')} type="text" id="email" />
-        </div>
-        <div className='registre'>
-            <label className='registre-text' htmlFor="Password">Password<br/></label>
-            <input className='registre-cuadro' {...register('password')} type="password" id="Password" />
-        </div>
-        <div className='registre'>
-            <label className='registre-text'  htmlFor="First_name">First name<br/></label>
-            <input className='registre-cuadro' {...register('first_name')} type="text" id="First_name" />
-        </div>
-        <div className='registre'>
-            <label className='registre-text' htmlFor="last_name">Last name<br/></label>
-            <input className='registre-cuadro' {...register('last_name')}  type="text" id="last_name" />
-        </div>
-        <div className='registre'>
-            <label className='registre-text' htmlFor="birthday">Birthday<br/></label>
+          </div>
+          <div className='registre'>
+            <label className='registre-text' htmlFor="password">Password<br /></label>
+            <input className='registre-cuadro' {...register('password')} type="password" id="password" />
+          </div>
+          <div className='registre'>
+            <label className='registre-text' htmlFor="first_name">First name<br /></label>
+            <input className='registre-cuadro' {...register('first_name')} type="text" id="first_name" />
+          </div>
+          <div className='registre'>
+            <label className='registre-text' htmlFor="last_name">Last name<br /></label>
+            <input className='registre-cuadro' {...register('last_name')} type="text" id="last_name" />
+          </div>
+          <div className='registre'>
+            <label className='registre-text' htmlFor="birthday">Birthday<br /></label>
             <input className='registre-cuadro' {...register('birthday')} type="date" id="birthday" />
+          </div>
         </div>
-        </div>
-        <button className='button-create'> {infoUpdate ? 'actulizar datos' :'registrar datos'} <br />{infoUpdate ? '🔁' :'▶️'} </button>
-    </form>
+        <button className='button-create'> {infoUpdate ? 'Update User' : 'Create User'} <br />{infoUpdate ? '🔁' : '▶️'} <br />  </button>
+      </form>
     </div>
   );
 };
 
-export default FormUser
+export default FormUser;
+
+
